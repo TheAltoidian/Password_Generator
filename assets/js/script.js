d9270c3c813e774e4213725ff8b473e5ac298d90
Cullen <cullenjcasey@gmail.com>
2022-01-18 22:59:07 -0800

    password requirements recieved from user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
 // Assignment code here
+var passReqs = {
+  length: 8,
+  lowerCase: false,
+  upperCase: false,
+  numeric: false,
+  specialChararacters: false,
+}
 
 var askLength = function() {
-  var passLength = parseInt(window.prompt("How long would you like your password? Choose an integer between 8 and 128, inclusive."));
-  if (passLength > 7 && passLength < 129) {
-    return passLength;
+  passReqs.length = parseInt(window.prompt("How long would you like your password? Choose an integer between 8 and 128, inclusive."));
+  if (passReqs.length > 7 && passReqs.length < 129) {
+    return passReqs.length;
   }
   else {
+    window.alert("Entered value not a number between 8 and 128, please try again.");
     return askLength();
+  }
+}
+
+var askReqs = function() {
+  lowerCase = window.confirm("Should the password contain lower case letters? \r\n YES: Okay \r\n NO: Cancel");
+  upperCase = window.confirm("Should the password contain upper case letters? \r\n YES: Okay \r\n NO: Cancel");
+  numeric = window.confirm("Should the password contain numbers? \r\n YES: Okay \r\n NO: Cancel");
+  specialChararacters = window.confirm("Should the password contain special characters? \r\n YES: Okay \r\n NO: Cancel");
+  if (!(lowerCase || upperCase || numeric || specialChararacters)) {
+    window.alert("Password must contain at least one type of character. Please select at least one type of character.");
+    askReqs();
   }
 }
 
@@ -15,7 +34,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var passLength = askLength();
+  askLength();
+  askReqs();
+  debugger;
+
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
