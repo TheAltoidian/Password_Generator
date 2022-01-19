2b5adb6d6010370d72606eaa4a9c9519e4bcaff2
Cullen <cullenjcasey@gmail.com>
2022-01-18 23:38:27 -0800

    began work on creating a random character, then went to bed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-// Assignment code here
+// DELETE FIRST 3 LINES, THEN CONTINUE ON LINE 49
+
+
+// password requirements object
 var passReqs = {
   length: 8,
   lowerCase: false,
@@ -7,6 +10,16 @@ var passReqs = {
   specialChararacters: false,
 }
 
+// strings to pull random characters from
+// var stringLowerCase = "abcdef"
+
+// function to generate a random numberic value
+var RNG = function(min, max) {
+  var value = Math.floor (Math.random() * (max - min + 1) + min);
+  return value;
+}
+
+// Ask for the desired password length
 var askLength = function() {
   passReqs.length = parseInt(window.prompt("How long would you like your password? Choose an integer between 8 and 128, inclusive."));
   if (passReqs.length > 7 && passReqs.length < 129) {
@@ -18,6 +31,7 @@ var askLength = function() {
   }
 }
 
+// Ask for which types of characters will be in the password
 var askReqs = function() {
   lowerCase = window.confirm("Should the password contain lower case letters? \r\n YES: Okay \r\n NO: Cancel");
   upperCase = window.confirm("Should the password contain upper case letters? \r\n YES: Okay \r\n NO: Cancel");
@@ -29,6 +43,25 @@ var askReqs = function() {
   }
 }
 
+// assign character types numbers for RNG selection
+var category = [lowerCase, upperCase, numeric, specialChararacters]
+
+// Create a random character from the given requirements
+var randomCharacter = function() {
+  while (!(category[RNG(0,3)])) {}
+  
+  // var category = RNG(0, 3);
+  console.log(category);
+}
+
+// Create the password
+var passwordGen = "";
+var generatePassword = function () {
+  for (let i = 0; i < passReqs.length; i++) {
+    passwordGen += randomCharacter();
+  }
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -36,9 +69,8 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   askLength();
   askReqs();
-  debugger;
 
-  // var password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
