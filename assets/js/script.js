dca08fa22afd52610bb2ab0713740b9b2d0b3bdf
Cullen <cullenjcasey@gmail.com>
2022-01-18 22:20:42 -0800

    check for length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 // Assignment code here
 
+var askLength = function() {
+  var passLength = parseInt(window.prompt("How long would you like your password? Choose an integer between 8 and 128, inclusive."));
+  if (passLength > 7 && passLength < 129) {
+    return passLength;
+  }
+  else {
+    return askLength();
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var passLength = askLength();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
