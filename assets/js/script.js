af5a1da6eddfe6132c9c9bca76392c45388d3fab
Cullen <cullenjcasey@gmail.com>
2022-01-19 15:16:03 -0800

    password created and displayed in the box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
-// DELETE FIRST 3 LINES, THEN CONTINUE ON LINE 49
-
-
 // password requirements object
 var passReqs = {
   length: 8,
   lowerCase: false,
   upperCase: false,
   numeric: false,
-  specialChararacters: false,
+  specialCharacters: false,
 }
 
 // strings to pull random characters from
-// var stringLowerCase = "abcdef"
+var stringLowerCase = "abcdefghijklmnopqrstuvwxyz";
+var stringUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var stringNumeric = "0123456789";
+var stringSpecialCharacters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~" + '"' + "\\"
 
-// function to generate a random numberic value
+// function to create a random numberic value
 var RNG = function(min, max) {
   var value = Math.floor (Math.random() * (max - min + 1) + min);
   return value;
@@ -33,33 +33,77 @@ var askLength = function() {
 
 // Ask for which types of characters will be in the password
 var askReqs = function() {
-  lowerCase = window.confirm("Should the password contain lower case letters? \r\n YES: Okay \r\n NO: Cancel");
-  upperCase = window.confirm("Should the password contain upper case letters? \r\n YES: Okay \r\n NO: Cancel");
-  numeric = window.confirm("Should the password contain numbers? \r\n YES: Okay \r\n NO: Cancel");
-  specialChararacters = window.confirm("Should the password contain special characters? \r\n YES: Okay \r\n NO: Cancel");
-  if (!(lowerCase || upperCase || numeric || specialChararacters)) {
+  passReqs.lowerCase = window.confirm("Should the password contain lower case letters? \r\n YES: Okay \r\n NO: Cancel");
+  passReqs.upperCase = window.confirm("Should the password contain upper case letters? \r\n YES: Okay \r\n NO: Cancel");
+  passReqs.numeric = window.confirm("Should the password contain numbers? \r\n YES: Okay \r\n NO: Cancel");
+  passReqs.specialCharacters = window.confirm("Should the password contain special characters? \r\n YES: Okay \r\n NO: Cancel");
+  if (!(passReqs.lowerCase || passReqs.upperCase || passReqs.numeric || passReqs.specialCharacters)) {
     window.alert("Password must contain at least one type of character. Please select at least one type of character.");
     askReqs();
   }
 }
 
-// assign character types numbers for RNG selection
-var category = [lowerCase, upperCase, numeric, specialChararacters]
+// Recieves characterType, then randomly returns a character of that type
+var characterPicker = function(cType) {
+  switch (cType) {
+    case passReqs.lowerCase:
+      return (stringLowerCase.charAt(RNG(0,25)));
+      break;
+    case passReqs.upperCase:
+      return (stringUpperCase.charAt(RNG(0,25)));
+      break;
+    case passReqs.numeric:
+      return (stringNumeric.charAt(RNG(0,9)));
+      break;
+    case passReqs.specialCharacters:
+      return (stringSpecialCharacters.charAt(RNG(0,32)));
+      break;
+  }
+}
 
-// Create a random character from the given requirements
+// Randomly picks the category for the next character in the password, then calls randomCharacter() to get the character itself
 var randomCharacter = function() {
-  while (!(category[RNG(0,3)])) {}
-  
-  // var category = RNG(0, 3);
-  console.log(category);
+  characterType = RNG(0,3);
+  switch (characterType) {
+    case 0:
+      if (passReqs.lowerCase) {
+        return (stringLowerCase.charAt(RNG(0,25)));
+      }
+      else {
+        return (randomCharacter());
+      }
+    case 1:
+      if (passReqs.upperCase){
+        return (stringUpperCase.charAt(RNG(0,25)));
+      }
+      else {
+        return (randomCharacter());
+      }
+    case 2:
+      if (passReqs.numeric) {
+        return (stringNumeric.charAt(RNG(0,9)));
+      }
+      else {
+        return (randomCharacter());
+      }
+    case 3:
+      if (passReqs.specialCharacters) {
+        return (stringSpecialCharacters.charAt(RNG(0,32)));
+      }
+      else {
+        return (randomCharacter());
+      }
+  }
 }
 
 // Create the password
-var passwordGen = "";
 var generatePassword = function () {
+  var passwordGen = "";
   for (let i = 0; i < passReqs.length; i++) {
     passwordGen += randomCharacter();
+    console.log(passwordGen);
   }
+  return passwordGen;
 }
 
 // Get references to the #generate element
@@ -69,12 +113,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   askLength();
   askReqs();
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
